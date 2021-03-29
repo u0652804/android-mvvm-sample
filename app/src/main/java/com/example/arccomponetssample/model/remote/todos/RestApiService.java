8c3ddb1aceba8eb766f6f188449a664fde2c4497
unknown <81156490+noah071@users.noreply.github.com>
2021-03-29 09:39:33 +0800

    add retrofit sample code

---
+++ app/src/main/java/com/example/arccomponetssample/model/remote/todos/RestApiService.java
@@ -0,0 +1,30 @@
+package com.example.arccomponetssample.model.remote;
+
+import com.example.arccomponetssample.model.data.Todos;
+
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.http.Body;
+import retrofit2.http.GET;
+import retrofit2.http.POST;
+import retrofit2.http.Path;
+
+public interface RestApiService {
+    // base url : https://jsonplaceholder.typicode.com/
+    // get url : https://jsonplaceholder.typicode.com/todos/1
+    // post url : https://jsonplaceholder.typicode.com/todos
+
+    @GET("todos/")
+//    Call<TodosWrapper> getTodosList();
+    Call<List<Todos>> getTodosList();
+
+    @GET("todos/1")    // set a path of get url
+    Call<Todos> getTodos();    // use Todos object to receive response data
+
+    @GET("todos/{id}") // use {} present params about input，@Path will use params to {} as child path
+    Call<Todos> getTodosById(@Path("id") int id);
+
+    @POST("todos")
+    Call<Todos> postTodos(@Body Todos todos);// use @Body present post data
+}
---
+++ app/src/main/java/com/example/arccomponetssample/model/remote/todos/RetrofitInstance.java
@@ -0,0 +1,28 @@
+package com.example.arccomponetssample.model.remote;
+
+import java.util.concurrent.TimeUnit;
+
+import okhttp3.OkHttpClient;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+public class RetrofitInstance {
+
+    private static Retrofit retrofit;
+    private static long TIME_OUT_SEC = 3;
+    private static String BASE_URL_TODOS = "https://jsonplaceholder.typicode.com/";
+
+    public static RestApiService getApiService(){
+        if (null == retrofit){
+            OkHttpClient client = new OkHttpClient().newBuilder()
+                    .connectTimeout(TIME_OUT_SEC, TimeUnit.SECONDS)   // 設置連線Timeout
+                    .build();
+            retrofit = new Retrofit.Builder()
+                    .client(client)
+                    .addConverterFactory(GsonConverterFactory.create())
+                    .baseUrl(BASE_URL_TODOS)
+                    .build();
+        }
+        return retrofit.create(RestApiService.class);
+    }
+}
